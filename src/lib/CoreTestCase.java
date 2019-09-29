923bdb5937be3ae4d4d39a18126ec6066fa75a81
Alex Popkov <poalrom@yandex-team.ru>
2019-09-29 14:39:38 +0300

    Упражнение 10

---
+++ src/lib/CoreTestCase.java
@@ -0,0 +1,48 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.ScreenOrientation;
+
+public class CoreTestCase extends TestCase
+{
+
+    protected AppiumDriver driver;
+    protected Platform Platform;
+
+    @Override
+
+    protected void setUp() throws Exception
+    {
+
+        super.setUp();
+        this.Platform = new Platform();
+        driver = this.Platform.getDriver();
+        this.rotateScreenPortrait();
+    }
+
+    @Override
+
+    protected void tearDown() throws Exception
+    {
+        driver.quit();
+
+        super.tearDown();
+    }
+
+    protected void rotateScreenPortrait()
+    {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+    }
+
+    protected void rotateScreenLanscape()
+    {
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+    }
+
+    protected void backgroundApp(int seconds)
+    {
+        driver.runAppInBackground(seconds);
+    }
+
+}
